072bee2f9cdb6a691716684f5a043a1d8ee0d0f7
lilybobj <156983048+lilybobj@users.noreply.github.com>
2024-03-04 00:35:45 -0800

    add files

---
+++ YummieApp.swift
@@ -0,0 +1,22 @@
+//
+//  YummieApp.swift
+//  Yummie
+//
+//  Created by Lily Jiang on 3/3/24.
+//
+
+import SwiftUI
+
+@main
+struct YummieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+            NavigationView{
+                WelcomeView()
+            }
+           
+            
+        }
+    }
+}
